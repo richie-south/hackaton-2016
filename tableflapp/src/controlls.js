9fc469ddd22a0847fd22e44941b03558741d0f72
rs222kn <rs222kn@student.lnu.se>
2016-12-09 20:00:16 +0100

    fixed pause button

--- tableflapp/src/controlls.js
+++ tableflapp/src/controlls.js
@@ -4,16 +4,16 @@ import store from './store'
 import * as actionsCreators from './actions/actionCreators'
 
 
-document.addEventListener('keypress', function (e) {
-  if(e.keyCode === 80){
-    console.log("asd")
-    store.dispatch(actionsCreators.turnGameOff())
-  }
+document.addEventListener('keydown', function (e) {
+  
   if(!store.getState().isGameOn){
     store.dispatch(actionsCreators.turnGameOn())
   }else if( e.keyCode === 32){
-    //store.dispatch(actionsCreators.movePlayerUp())
     store.dispatch(actionsCreators.jump())
+  }
+
+  if(e.keyCode === 80){
+    store.dispatch(actionsCreators.turnGameOff())
   }
 
 })
