b246c17d701886153853da3be005fef6527d1ae2
Benny Carlsson <carlsson.benny93@gmail.com>
2016-12-10 01:59:54 +0100

    tried some kinda of fps cap?

--- tableflapp/src/gameLogic.js
+++ tableflapp/src/gameLogic.js
@@ -3,23 +3,33 @@ import store from './store'
 import * as actionsCreators from './actions/actionCreators'
 
 let jumpCount = 0
+let lastTime;
 function render(){
+  if(!lastTime){
+    lastTime = new Date().getTime()
+  }
+  let now = new Date().getTime()
+  let timeSinceLast = now - lastTime
 
   const currentState = store.getState()
 
   if(currentState.isGameOn){
-
+    if(timeSinceLast < 30){
+      return
+    }
+    lastTime = new Date().getTime();
     if(currentState.currentPlayerPosition.find(a => a.row > 50 || a.row < 0)){
       store.dispatch(actionsCreators.gameOver())
       store.dispatch(actionsCreators.turnGameOff())
     }
-
 
     const pillarPos = currentState.currentPillarPosition[0]
     if(pillarPos.column < -3){
       store.dispatch(actionsCreators.reusePillar())
       store.dispatch(actionsCreators.increaseScore())
     }
+
+
 
 
     currentState.currentPlayerPosition.forEach(playerPosition => {
@@ -33,7 +43,6 @@ function render(){
       }
     })
 
-
     if(currentState.goingUp){
       if(jumpCount > 10){
           store.dispatch(actionsCreators.stopJump())
@@ -45,7 +54,6 @@ function render(){
       store.dispatch(actionsCreators.movePlayerDown())
     }
     store.dispatch(actionsCreators.pillarMovement())
-    //store.dispatch(actionsCreators.movePlayerUp())
 
   }
 }
@@ -53,6 +61,9 @@ function render(){
 
 
 (function animloop(){
-  window.requestAnimationFrame(animloop)
-  render()
+  //setTimeout( ()=>{
+    window.requestAnimationFrame(animloop)
+    render()
+  //},1000/60)
+
 })()
